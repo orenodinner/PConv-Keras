36faec4543ab701060b09a0f574e036e69483072
oreno_dinner <orenodinner@gmail.com>
2019-02-04 09:54:29 +0900

    Update pconv_model.py

--- libs/pconv_model.py
+++ libs/pconv_model.py
@@ -233,7 +233,7 @@ class PConvUnet(object):
         print(self.model.summary())
 
     def save(self):        
-        self.model.save_weights(self.current_weightfile(),save_format="h5")
+        self.model.save_weights(self.current_weightfile())
 
     def load(self, filepath, train_bn=True, lr=0.0002):
 
