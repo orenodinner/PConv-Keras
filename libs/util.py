f49329d490101584230796b11d33a81ac78a15a5
oreno_dinner <orenodinner@gmail.com>
2019-02-08 16:25:16 +0900

    Update util.py

--- libs/util.py
+++ libs/util.py
@@ -39,5 +39,5 @@ def random_mask(height, width, channels=3):
 def green_mask(img_og,height, width, channels=3):
     img_mask = np.zeros((height, width, channels), np.uint8)
     img_mask2 = cv2.inRange(img_og, (0,255,0), (0,255,0))
-    img_mask[0:height, 0:width] = img_mask2
+    img[:,:,0], img[:,:,1], img[:,:,2] = img_mask2
     return 1- img_mask
